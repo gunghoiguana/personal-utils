631bf251274dfdbff84637d12488a08aec101c65
gunghoiguana <98073123+gunghoiguana@users.noreply.github.com>
2022-01-19 22:59:16 -0800

    auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React from 'react';
 import './App.css';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Let's geet personal
-        </p>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <p>
+            Hey {user.username}, welcome to my channel, with auth!
+          </p>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
